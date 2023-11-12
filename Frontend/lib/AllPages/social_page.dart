0d30a6bfbf852d2d3c0fd6ffd243a2a43c589406
thenamk20 <namlaophu@gmail.com>
2023-11-12 14:46:06 +0700

    sample pages + bottom bar navigation

---
+++ Frontend/lib/AllPages/social_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SocialPage extends StatefulWidget {
+  const SocialPage({super.key});
+
+  @override
+  State<SocialPage> createState() => _SocialPageState();
+}
+
+class _SocialPageState extends State<SocialPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Social Page"),
+      ),
+    );
+  }
+}
